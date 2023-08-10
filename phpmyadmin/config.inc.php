0a3ff1cd0773db4bb316a849c29a098f0328fdfb
jsoliz064 <jsoliz064@gmail.com>
2023-08-10 14:49:26 +0000

    first commit

---
+++ phpmyadmin/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+$cfg['Servers'][1]['host'] = '';
+$cfg['Servers'][1]['port'] = '3306';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = '';
+$cfg['Servers'][1]['auth_type'] = 'cookie';
+$cfg['Servers'][1]['AllowNoPassword'] = false;
+$cfg['Servers'][1]['AllowUserDropDatabase'] = true;
+$cfg['Servers'][1]['AllowArbitraryServer'] = true;
+?>
+
